01e4b3238c5d33e00c8a86fe7abc411bb5296bdb
Kseniya Potapovich <ksuy.potapovich@bk.ru>
2023-10-06 16:47:15 +0300

    Add second homework)

---
+++ src/Homework2.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+
+public class Homework2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        /** Task 1  */
+        /*int n = scanner.nextInt();
+        if (n % 2 == 0) {
+            System.out.println("Число четное");
+        } else System.out.println("Число нечетное");*/
+
+        /** Task 2 */
+        /*int t = scanner.nextInt();
+        if (t > -5) {
+            System.out.println("Warm");
+        } else if (t <= -5 && t > -20) {
+            System.out.println("Normal");
+        } else if (t <= -20) {
+            System.out.println("Cold");
+        }*/
+
+        /** Task 3 */
+        /*for (int i = 10; i<=20;i++){
+            System.out.println(i + "^2 = " + i*i);
+        }*/
+
+        /** Task 4 */
+        /*int a = 7;
+        while (a <= 98) {
+            System.out.print(a + " ");
+            a += 7;
+        }*/
+
+        /** Task with * */
+        /*int s = 0;
+        if (scanner.hasNextInt()) {
+            s = scanner.nextInt();
+        }
+        int sum = 0;
+        for (int i = 0; i <= s; i++) {
+            sum += i;
+        }
+        System.out.println(sum);*/
+    }
+}
