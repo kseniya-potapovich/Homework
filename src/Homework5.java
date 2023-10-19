c6c97f9915c111741bb136d7f0893b0212f14c31
Kseniya Potapovich <ksuy.potapovich@bk.ru>
2023-10-19 10:58:41 +0300

    Bingo!

--- src/Homework5.java
+++ src/Homework5.java
@@ -21,11 +21,11 @@ public class Homework5 {
             System.out.println();
         }*/
         System.out.println("Введите число");
-        int n = scanner.nextInt();
+        int k = scanner.nextInt();
         int sum = 0;
         for (int i = 0; i < size; i++) {
             for (int j = 0; j < size; j++) {
-                array[i][j] += n;
+                array[i][j] += k;
                 sum += array[i][j];
             }
         }
@@ -45,6 +45,34 @@ public class Homework5 {
         for (int i = 0; i < 8; i++) {
             for (int j = 0; j < 8; j++) {
                 System.out.print(stringArray[i][j] + " ");
+            }
+            System.out.println();
+        }
+        /** Task with * */
+        int n = scanner.nextInt();//строки
+        int m = scanner.nextInt();//столбцы
+        int[][] A = new int[n][m];
+        int count = 0;
+        //int row=0;
+        int col = 0;
+
+        for (int i = 0; i < n; i++) {
+            if (i % 2 == 0) {
+                for (; col < m; col++) {
+                    A[i][col] = count;
+                    count++;
+                }
+            } else {
+                for (; col > 0; col--) {
+                    A[i][col - 1] = count;
+                    count++;
+                }
+            }
+        }
+
+        for (int r = 0; r < n; r++) {
+            for (int e = 0; e < m; e++) {
+                System.out.print(A[r][e] + "\t");
             }
             System.out.println();
         }
